5b20ba1862617b0700e8c89f90081c9b0e112ba9
jbelkerf <jbelkerf@gmail.com>
2025-01-03 23:30:13 +0100

    add

--- pipex.c
+++ pipex.c
@@ -6,14 +6,18 @@
 /*   By: jbelkerf <jbelkerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 18:23:37 by jbelkerf          #+#    #+#             */
-/*   Updated: 2025/01/03 22:03:27 by jbelkerf         ###   ########.fr       */
+/*   Updated: 2025/01/03 23:29:31 by jbelkerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 #include "./libft/libft.h"
 
-
+void error(void)
+{
+	perror("Error");
+	exit(EXIT_FAILURE);
+}
 void exec_parent(char *argv[], char *envp[], int *pipefd, int cmd_numb)
 {
 	char	*cmd;
@@ -22,14 +26,16 @@ void exec_parent(char *argv[], char *envp[], int *pipefd, int cmd_numb)
 	cmd = check_cmd(argv[cmd_numb + 1], envp);
 	
 	close(pipefd[1]);
-	fd = open(argv[4], O_TRUNC | O_WRONLY);
-	dup2(fd, 1);
+	fd = open(argv[4], O_TRUNC | O_WRONLY | O_CREAT, 0777);
+	if (fd == -1)
+		error();
+	dup2(fd, STDOUT_FILENO);
 	//close(fd);
-	dup2(pipefd[0], 0);
-	execve(cmd, ft_split(argv[cmd_numb + 1], ' '), NULL);
-	ft_putstr_fd("pipex: command not found", 2);
-	exit(1);
+	dup2(pipefd[0], STDIN_FILENO);
+	execve(cmd, ft_split(argv[cmd_numb + 1], ' '), envp);
+	error();
 }
+	
 void exec_child(char *argv[], char *envp[], int *pipefd, int cmd_numb)
 {
 	char	*cmd;
@@ -38,13 +44,14 @@ void exec_child(char *argv[], char *envp[], int *pipefd, int cmd_numb)
 	cmd = check_cmd(argv[cmd_numb + 1], envp);
 	
 		close(pipefd[0]);
-		fd = open(argv[1], O_RDONLY);
-		dup2(fd, 0);
+		fd = open(argv[1], O_RDONLY, 0777);
+		if (fd == -1)
+			error();
+		dup2(fd, STDIN_FILENO);
 		//close(fd);
-		dup2(pipefd[1], 1);
-	execve(cmd, ft_split(argv[cmd_numb + 1], ' '), NULL);
-	ft_putstr_fd("pipex: command not found", 2);
-	exit(1);
+		dup2(pipefd[1], STDOUT_FILENO);
+	execve(cmd, ft_split(argv[cmd_numb + 1], ' '), envp);
+	error();
 }
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -52,20 +59,23 @@ int	main(int argc, char *argv[], char *envp[])
 	int	pipefd[2];
 
 	if (argc != 5)
+	{
 		exit(1);
+	}
 	if (pipe(pipefd) == -1)
 	{
-		ft_putstr_fd("pipex: command not found", 2);
-		exit(128);
+		error();
 	}
 	pid = fork();
 	if (pid == 0)
 		exec_child(argv, envp, pipefd, 1);
 	else if (pid > 0)
+	{
+		waitpid(pid, NULL, 0);
 		exec_parent(argv, envp, pipefd, 2);
+	}
 	else
 	{
-		ft_putstr_fd("cant fork", 2);
-		exit(1);
+		error();
 	}
 }
