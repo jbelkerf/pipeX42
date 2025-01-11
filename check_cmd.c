a8ea515e63ce3921cf41496f2bb48c364a4afeb2
errouihinisrine <errouihi.nisrine@etu.uae.ac.ma>
2025-01-11 22:29:52 +0100

    add

--- check_cmd.c
+++ check_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbelkerf <jbelkerf@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ner-roui <ner-roui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 18:20:33 by jbelkerf          #+#    #+#             */
-/*   Updated: 2025/01/11 15:18:30 by jbelkerf         ###   ########.fr       */
+/*   Updated: 2025/01/11 22:08:48 by ner-roui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	command_not_found(char *cmd)
 	ft_putstr_fd("JBash: ", 2);
 	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(": command not found\n", 2);
-	free(cmd);
+	//free(cmd);
 	exit(127);
 }
 
@@ -42,22 +42,19 @@ void	free_array(char **str)
 	free(str);
 }
 
-char	**extract_pathvariable(char **envp)
+char	*extract_pathvariable(char **envp)
 {
-	char	*path_var;
-	char	**paths;
 	int		i;
 
 	i = 0;
 	while (envp[i])
 	{
 		if (!ft_strncmp("PATH", envp[i], 4))
-			path_var = envp[i];
+			return (*(envp + i ) + 5);
 		i++;
 	}
-	path_var = path_var +5;
-	paths = ft_split2(path_var, ':');
-	return (paths);
+	
+	return (NULL);
 }
 
 char	*check_cmd(char *cmd, char **envp)
@@ -68,22 +65,24 @@ char	*check_cmd(char *cmd, char **envp)
 	char	*pcmd;
 	char	*tmp;
 
-	i = 0;
+	i = -1;
 	paths = ft_split(cmd, ' ');
 	pcmd = ft_strdup(paths[0]);
 	cmd = pcmd;
-	free_array(paths);
-	paths = extract_pathvariable(envp);
-	while (paths[i])
+	//free_array(paths);
+	tmp = extract_pathvariable(envp);
+	if (tmp == NULL)
+		return NULL;
+	paths = ft_split2(tmp , ':');
+	while (paths[++i])
 	{
 		tmp = paths[i];
 		paths[i] = ft_strjoin(paths[i], cmd);
-		free(tmp);
+		//free(tmp);
 		fd = access(paths[i], X_OK);
 		if (fd != -1)
 			return (paths[i]);
-		i++;
 	}
-	free_array(paths);
+	//free_array(paths);
 	return (command_not_found(pcmd), NULL);
 }
